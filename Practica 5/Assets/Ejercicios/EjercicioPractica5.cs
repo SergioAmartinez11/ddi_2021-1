859928f25b2d80fbe478ef76c094a7a952289cbd
Sergio <alonso.martinez64@uabc.edu.mx>
2021-03-24 18:56:41 -0700

    Correcion de ejercicio 2

--- Practica 5/Assets/Ejercicios/EjercicioPractica5.cs
+++ Practica 5/Assets/Ejercicios/EjercicioPractica5.cs
@@ -9,54 +9,55 @@ public class EjercicioPractica5 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int[] nums1 = {1,23,356,1,2340};
-        int[] nums2 = {4,1,2,1,2};
-        Debug.Log("Ejercicio 1:");
-        Debug.Log(numParDigitos(nums1));
-        Debug.Log("Ejercicio 2:");
-        Debug.Log(numParesEncontrados(nums2));
+        List<int> lista1 = new List<int>();
+        List<int> lista2 = new List<int>();
+        lista1.Add(1);
+        lista1.Add(23);
+        lista1.Add(356);
+        lista1.Add(2340);
+        lista2.Add(4);
+        lista2.Add(1);
+        lista2.Add(2);
+        lista2.Add(1);
+        lista2.Add(2);
+
+      //  Debug.Log("Ejercicio 1:");
+       // Debug.Log(numParDigitos(lista1));
+        Debug.Log("Ejercicio 2. El numero diferente es: ");
+        Debug.Log(numParesEncontrados(lista2));
 
     }
 
-    // Update is called once per frame
-    int numParDigitos(int[] nums)
-    {
+    // corregir
+    int numParDigitos(List<int> nums)
+    {   
         int result = 0;
-        int x;
-        int divisor = 10;
-        int comparador = 0;
-
-        for(int i=0;i<nums.Length;i++)
+        nums.Sort();
+       /* for(int i=0; i<nums.Count;i++)
         {
-            divisor = 10;
-            x = nums[i]/divisor;
-
-            while(x > comparador)
-            {
-                result +=1;
-                divisor = divisor*100;
-                comparador *= 100;
-                x = nums[i]/divisor;      
-            }
-        }
+            
+        }*/
+       
         return result;      
     }
-    int numParesEncontrados(int[] nums)
+    int numParesEncontrados(List<int> nums)
     {
-        int pos = -1;
-        int x;
+        nums.Sort();
+        int x,y;
 
-        for(int i=0;i < nums.Length;i++)
-        {   
+        x = nums[0];
+        y = nums[nums.Count-1];
+        if(x != nums[1])
+            return x;
+        if(y != nums[nums.Count-2])
+            return y;
+
+        for(int i=1;i < nums.Count-1; i++)
+        {
             x = nums[i];
-            for(int j = i+1;j<nums.Length;j++)
-            {
-                if(x != nums[j])
-                {
-                    pos = i;    
-                }
-            }
+            if(x != nums[i-1] && x != nums[i+1])
+                return x;
         }
-        return nums[pos];
-    }
+        return 0;
+    }   
 }
