f28023684f698b070a929ef4eac4d7a22dcaeae6
Sergio <alonso.martinez64@uabc.edu.mx>
2021-05-11 23:02:59 -0700

    Practica 8 interaccion por voz

---
+++ Practica 8/Assets/LanguageTranslationDemo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using IBM.Watson;
+
+public class LanguageTranslationDemo : MonoBehaviour
+{
+   public Text ResponseTextField;
+   private ExampleLanguageTranslatorV3 languageTranslator;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+   
+}
---
+++ Practica 8/Assets/changeColorSpech.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Renderer cubo;
+    private string speachColor;
+    void Start()
+    {
+        cubo = GetComponent<Renderer>();
+
+        cubo.material.SetColor("_Color",Color.black);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(speachColor == "azul"){
+             cubo.material.SetColor("_Color",Color.blue);
+        }
+        if(speachColor == "verde"){
+             cubo.material.SetColor("_Color",Color.green);
+        }
+        if(speachColor == "rojo"){
+             cubo.material.SetColor("_Color",Color.red);
+        }
+        else{
+             cubo.material.SetColor("_Color",Color.black);
+        }
+        
+    }
+    public void ReadStringInput(string s){
+        speachColor = s;
+    }
+}
